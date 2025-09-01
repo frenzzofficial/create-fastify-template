5cb46f6f112a2dd55032cc8beb39f6a4033769ff
vivek <vivekcse.in@gmail.com>
2025-09-01 22:38:27 +0530

    routes

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,34 @@ import type { FastifyInstance } from "fastify";
 
 
 const createApp = async () => {
-  // const app = Fastify();
   const app: FastifyInstance = Fastify({
     logger: {
       level: process.env.NODE_ENV === "production" ? "warn" : "info",
     },
   });
 
-  app.get("/", async (req, reply) => {
-    return { message: "obrigado" };
+  app.get("/", async (_req, reply) => {
+    return reply
+      .status(200)
+      .type("text/html")
+      .send("<h1>Hello from Fastify!</h1>");
   });
-  app.get("/api/hello", async (req, reply) => {
-    return { message: "Hello from Fastify on Vercel!" };
+
+  app.get("/api/health", (_req, reply) => {
+    return reply.status(200).send({
+      success: true,
+      message: "Server is healthy",
+      timestamp: new Date().toISOString(),
+      environment: process.env.NODE_ENV,
+    });
+  });
+
+  // 404 handler
+  app.setNotFoundHandler((_request, reply) => {
+    return reply.status(404).send({
+      success: false,
+      message: "Route not found",
+    });
   });
 
   return app;
