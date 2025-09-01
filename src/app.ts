1e2104a3b5789417aa61c09420602ce9aea57cea
vivek <vivekcse.in@gmail.com>
2025-09-01 22:48:49 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
 import type { FastifyInstance } from "fastify";
+import { configViews, Home } from "./libs/configs/config.view";
 
 
 const createApp = async () => {
@@ -9,11 +11,15 @@ const createApp = async () => {
     },
   });
 
+  //register all plugins
+  //serve static files
+  await app.register(fastifyStatic, configViews);
+
   app.get("/", async (_req, reply) => {
     return reply
       .status(200)
       .type("text/html")
-      .send("<h1>Hello from Fastify!</h1>");
+      .sendFile(Home);
   });
 
   app.get("/api/health", (_req, reply) => {
@@ -37,43 +43,3 @@ const createApp = async () => {
 };
 
 export default createApp;
-
-// const createApp = async () => {
-//   const app: FastifyInstance = Fastify({
-//     logger: {
-//       level: process.env.NODE_ENV === "production" ? "warn" : "info",
-//     },
-//   });
-
-//   app.get("/", async (_req, reply) => {
-//     return reply
-//       .status(200)
-//       .type("text/html")
-//       .send("<h1>Hello from Fastify!</h1>");
-//   });
-
-//   app.get("/api/health", (_req, reply) => {
-//     return reply.status(200).send({
-//       success: true,
-//       message: "Server is healthy",
-//       timestamp: new Date().toISOString(),
-//       environment: process.env.NODE_ENV,
-//     });
-//   });
-
-//   // 404 handler
-//   app.setNotFoundHandler((_request, reply) => {
-//     return reply.status(404).send({
-//       success: false,
-//       message: "Route not found",
-//     });
-//   });
-
-//   return app;
-// };
-
-// Ensure the promise is handled
-// const appInstance = createApp();
-
-// export default appInstance;
-
